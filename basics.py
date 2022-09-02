b94d139da2fa436ec8eebbc825ccaf6e54ec2a89
Sharvin27 <112688310+Sharvin27@users.noreply.github.com>
2022-09-02 13:54:00 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,37 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x=10
+x
+10
+2+3=5
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+2+3
+5
+5/2
+2.5
+5//2
+2
+2**3
+8
+5**3
+125
+10//3
+3
+10%3
+1
+'Sharvin'
+'Sharvin'
+print("Sharvin")
+Sharvin
+10*Sharvin
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    10*Sharvin
+NameError: name 'Sharvin' is not defined
+10*'Sharvin'
+'SharvinSharvinSharvinSharvinSharvinSharvinSharvinSharvinSharvinSharvin'
+print('Sharvin\navin')
+Sharvin
+avin
+print(r'Sharvin\navin')
+Sharvin\navin
